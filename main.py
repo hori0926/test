23f526bbc319b5f8d236ab6866a8f4515c0fb261
hori0926 <hirotaka.h09@gmail.com>
2021-01-23 09:29:23 +0900

    log

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask,request,abort
+from flask.logging import create_logger
 from linebot import LineBotApi,WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent,TextMessage,TextSendMessage
 import os
 
 app=Flask(__name__)
+LOG = create_logger(app)
 
 YOUR_CHANNEL_ACCESS_TOKEN = "WXxwsnhHb8dZ9UCl7mgV1vx4js8887TMjYwiDZh+jXLapUOi/7isvtcOmZP9m+prCkf1A4hpCMlBfNaRPTAiCzid+mPvNMhcpnfHPHz98cxR2bTTJCjghADZnyk7JOjWFYJVC14reFW39QdF6p607gdB04t89/1O/w1cDnyilFU="
 YOUR_CHANNEL_SECRET = "U51f48c4a84fe544502cf3f944737c16e"
@@ -15,9 +17,21 @@ handler=WebhookHandler(YOUR_CHANNEL_SECRET)
 def hello_world():
     return "hello world!!!"
 
-@app.route("/callback")
+@app.route("/callback", methods=['POST'])
 def callback():
-    return "hello callback!"
+    # リクエストヘッダーから署名検証のための値を取得
+    signature = request.headers['X-Line-Signature']
+
+    # リクエストボディを取得
+    body = request.get_data(as_text=True)
+    LOG.info("Request body: " + body)
+
+    # 署名を検証し、問題なければhandleに定義されている関数を呼ぶ
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):

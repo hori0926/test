e7a6861b107dce82d930a70eb8f8f7d31340e884
hori0926 <hirotaka.h09@gmail.com>
2021-01-23 10:39:12 +0900

    hello

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
     return 'OK'
 
